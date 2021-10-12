f4376421845febb3206fb3da028cc25627a61bce
Gerald Gonzalez <gedgonz7@gmail.com>
2021-10-12 17:33:37 -0600

    new component Proyects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Initial from "./components/home/Initial";
 import Menu from "./components/nav/Menu";
 import Aboutme from "./components/about/Aboutme";
+import Proyects from "./components/proyects/Proyects";
 function App() {
   return (
     <div className="App">
       <Menu/>
       <Initial className="mt-10"/>
       <Aboutme />
+      <Proyects />
     </div>
   );
 }
---
+++ src/components/proyects/Proyects.jsx
@@ -0,0 +1,185 @@
+import React from 'react'
+import  "./Proyects.css";
+export default function Proyects() {
+    return (
+        <section id="proyects">
+            <div className="container">
+                <br/> <br/> <br/> <br/>
+                <div className="about text-center">
+                    <h3>Estos son algunos de mis proyectos</h3>
+                </div>
+                <br/> <br/> <br/> <br/>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="images-right">
+
+                            <img className="picture_proyect" src="https://repository-images.githubusercontent.com/351929749/fd248500-9de6-11eb-94df-a0ad560e771c" alt="" />
+
+                        </div>
+                        <div className="contents-left">
+                            <p className="title_info_one">BlueBank </p>
+                            <p className="text-justify">
+                                Estas es una pequeña aplicacion web que realiza la gestion de cuenta de ahorro de un banco que se conecta para consumir los datos de una API desarrollada en .NetCore,
+                                el backend utiliza una arquitectura limpia, con patrones de diseño, con los principios DDD, gestion de logs y el uso de JWT
+                            </p>
+                            <div className="text-center Detailproyect">
+                                <i className="devicon-typescript-original colored"></i>
+                                <i className="devicon-angularjs-plain colored"></i>
+                                <i className="devicon-dotnetcore-plain colored"></i>
+                            </div>
+                            <br/>
+                            <div className="text-center">
+                                <a href="https://github.com/GedGonz/BlueBank" className="btn btn-light btn-lg "><i className="fa fa-github"></i></a>
+                                <a href="https://bluebankapp.herokuapp.com" className="btn btn-light btn-lg "><i className="fa fa-eye"></i></a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <br/> <br/> <br/> <br/>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="images-left">
+
+                            <img className="picture_proyect" src="https://repository-images.githubusercontent.com/223289504/594c5d00-bc4a-11ea-932b-2273e258bb6e" alt="" />
+
+                        </div>
+                        <div className="contents-right">
+                            <p className="title_info_one">FunkoPop </p>
+                            <p className="text-justify">
+                                Esta es una pequeña aplicación donde se con una landingpage donde muestra un careusel de productos Funko Pop, 
+                                asi mismo un buscador en donde se demuestra la comunicacion entre componentes, esta consume una api creada en NodeJS
+                            </p>
+                            <div className="text-center Detailproyect">
+                                <i className="devicon-typescript-original colored"></i>
+                                <i className="devicon-angularjs-plain colored"></i>
+                                <i className="devicon-mongodb-plain colored"></i>
+                            </div>
+                            <br/>
+                            <div className="text-center">
+                                <a href="https://github.com/GedGonz/TiendaFunkoPOP" className="btn btn-light btn-lg "><i className="fa fa-github"></i></a>
+                                <a href="https://funkopopmarket.herokuapp.com" className="btn btn-light btn-lg "><i className="fa fa-eye"></i></a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <br/> <br/> <br/> <br/>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="images-right">
+
+                            <img className="picture_proyect" src="https://repository-images.githubusercontent.com/381210182/c9263997-0b9e-4fab-ab46-61432263b5d1" alt="" />
+
+                        </div>
+                        <div className="contents-left">
+                            <p className="title_info_one">Rick&Morty </p>
+                            <p className="text-justify">
+                                Esta es una pequeña aplicación donde hace la buqueda de los personajes de la famosa serie de Rick y Morty!.
+                                en esta app se demuestra el consumo de una api y el scroll infinito.
+                            </p>
+                            <div className="text-center Detailproyect">
+                                <i className="devicon-typescript-original colored"></i>
+                                <i className="devicon-angularjs-plain colored"></i>
+                            </div>
+                            <br/>
+                            <div className="text-center">
+                                <a href="https://github.com/GedGonz/AppRickAndMorty" className="btn btn-light btn-lg"><i className="fa fa-github"></i></a>
+                                <a href="https://gedgonz.github.io/AppRickAndMorty" className="btn btn-light btn-lg "><i className="fa fa-eye"></i></a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <br/> <br/> <br/> <br/>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="images-left">
+                            <img className="picture_proyect" src="https://repository-images.githubusercontent.com/55745086/2c4c7a00-bc4b-11ea-897e-18121dd77df5" alt="" />
+
+                        </div>
+                        <div className="contents-right">
+                            <p className="title_info_one">BlogJS </p>
+                            <p className="text-justify">
+                                Esta aplicación hace la gestion y  creacion de post sobre el tema que tu prefieras, esta aplicacion se desarrollo en NodeJS, Express y Mongodb
+                            </p>
+                            <div className="text-center Detailproyect">
+                                <i className="devicon-javascript-plain colored"></i>
+                                <i className="devicon-nodejs-plain colored"></i>
+                                <i className="devicon-mongodb-plain colored"></i>
+                            </div>
+                            <br/>
+                            <div className="text-center">
+                                <a href="https://github.com/GedGonz/BLOGJS" className="btn btn-light btn-lg"><i className="fa fa-github"></i></a>
+                                <a href="https://blogj.glitch.me" className="btn btn-light btn-lg "><i className="fa fa-eye"></i></a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <br/> <br/> <br/> <br/>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="images-right">
+
+                            <img className="picture_proyect" src="https://repository-images.githubusercontent.com/194757099/4846a600-89a9-11eb-9926-2c9bb154c386" alt="" />
+
+                        </div>
+                        <div className="contents-left">
+                            <p className="title_info_one">Github API </p>
+                            <p className="text-justify">
+                                En esta aplicación hace uso de la api de github en donde al buscar un usuario muestra sus datos principales y sus repositorios.
+                            </p>
+                            <div className="text-center Detailproyect">
+                                <i className="devicon-typescript-original colored"></i>
+                                <i className="devicon-angularjs-plain colored"></i>
+                                <i className="devicon-github-plain colored"></i>
+                            </div>
+                            <br/>
+                            <div className="text-center">
+                                <a href="https://github.com/GedGonz/AngularAPIGitHub" className="btn btn-light btn-lg "><i className="fa fa-github"></i></a>
+                                <a href="https://appfindrepository.herokuapp.com" className="btn btn-light btn-lg "><i className="fa fa-eye"></i></a>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <br/> <br/> <br/> <br/>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="images-left">
+
+                            <img className="picture_proyect" src="https://repository-images.githubusercontent.com/402940655/bd0c910d-370d-42cc-a9a6-9d4a7929e043" alt="" />
+
+                        </div>
+                        <div className="contents-right">
+                            <p className="title_info_one">React Movies </p>
+                            <p className="text-justify">
+                                Esta es una pequeña app que la desarrolle como practica de estudio sobre reactJS, es una aplicación donde muestra el top de peliculas, buscador y un detalle de cada una.
+                            </p>
+                            <div className="text-center Detailproyect">
+                                <i className="devicon-react-original colored"></i>
+                            </div>
+                            <br/>
+                            <div className="text-center">
+                                <a href="https://github.com/GedGonz/reactMovie" className="btn btn-light btn-lg"><i className="fa fa-github"></i></a>
+                                <a href="https://gedgonz.github.io/reactMovie/" className="btn btn-light btn-lg "><i className="fa fa-eye"></i></a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="presentation">
+                <h3 className=" pt-lg-30 text-center"><span className="typed" /></h3>
+            </div>
+            <footer>
+                <div className="wave-container">
+                    <svg id='canvas' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,128L60,138.7C120,149,240,171,360,160C480,149,600,107,720,85.3C840,64,960,64,1080,64C1200,64,1320,64,1380,64L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+
+                </div>
+            </footer>
+        </section>
+    )
+}
