eab38299dcdca20dfa17486f929dcad36f0e1386
Gerald Gonzalez <gedgonz7@gmail.com>
2021-10-13 14:22:30 -0600

    style was added to the scroll bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Initial from "./components/home/Initial";
 import Menu from "./components/nav/Menu";
 import Aboutme from "./components/about/Aboutme";
 import Proyects from "./components/proyects/Proyects";
+import './App.css';
 function App() {
   return (
     <div className="App">
